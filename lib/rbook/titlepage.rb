30941f57cd99a45a48f5a327aabe16fc25e6b841
yob <yob@ec8fb974-7d1c-0410-aeba-c8cafba90cbb>
2007-09-17 10:53:50 +0000

    titlepage: - use the soap4r gem if a local system has it, as it's often better than the ruby stdlib one
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rbook/titlepage/trunk@103 ec8fb974-7d1c-0410-aeba-c8cafba90cbb

--- lib/rbook/titlepage.rb
+++ lib/rbook/titlepage.rb
@@ -1,5 +1,9 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__) + "/../")
 
+# the soap4r gem is often a little better than the version that lives in
+# the ruby standard library, so if the local system has the gem installed 
+# use that
+begin; gem 'soap4r'; rescue; end
 require 'rbook/isbn'
 require 'rbook/titlepage/titlepage_driver'
 require 'rbook/titlepage/client'
