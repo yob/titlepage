201f342a6e2b9507f71dfc8da33920377f09a494
James Healy <jimmy@deefa.com>
2008-09-08 21:47:26 +1000

    tidy up examples dir

--- examples/titlepage_www.rb
+++ examples/titlepage_www.rb
@@ -1,18 +1,11 @@
-# assuming you have rbook installed via rubygems,
-# in a regular script, replace the following require
-# line with these 2 lines:
-#   require 'rubygems'
-#   require 'rbook/titlepage'
-require File.dirname(__FILE__) + '/../lib/rbook/titlepage'
-require File.dirname(__FILE__) + '/../lib/rbook/onix'
+require 'rubygems'
+require File.dirname(__FILE__) + '/../lib/titlepage'
 
-client = RBook::TitlePage::WWWClient.new
+client = TitlePage::WWWClient.new
 
 client.login("username", "password")
 
-  text = client.get_onix_file("9780060000707")
-  #msg = RBook::Onix::Message.load_from_string(text)
-  #puts msg.to_s
-  puts text
+text = client.get_onix_file("9780060000707")
+puts text
 
 client.logout
